a5e7f44d108c55159aedcaa0b72468fddff88548
0788763 <0788763@KELCE-LABS26-P.pittstate.edu>
2019-10-15 11:38:10 -0500

    init commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 0788763
+ */
+public class Main 
+{
+    public static void main(String[] args) 
+    {
+        Scanner keyboard = new Scanner(System.in);
+        
+        int test = keyboard.nextInt();
+    }
+}
