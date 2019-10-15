f2fe8cbcd42662effbaa1d41a0f58056834a63b5
0788763 <0788763@KELCE-LABS26-P.pittstate.edu>
2019-10-15 12:00:57 -0500

    try-catch

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,5 @@
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 /*
@@ -6,17 +7,35 @@ import java.util.Scanner;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author 0788763
  */
 public class Main 
 {
+
     public static void main(String[] args) 
     {
         Scanner keyboard = new Scanner(System.in);
-        
-        int test = keyboard.nextInt();
+
+        System.out.println("Please enter a number");
+        try 
+        {
+            int test = keyboard.nextInt();
+            System.out.println(test + " is a good number");
+        } 
+        catch (InputMismatchException x) 
+        {
+            System.out.println("That's not a number");
+        }
+        catch (Exception z)
+        {
+            System.out.println("An unknown error occured");
+            System.out.println(z.getMessage());
+        }
+        finally
+        {
+            System.out.println("This is the end of the program");
+        }
     }
 }
